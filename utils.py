17acfc1c662fafe2020592afcce64c5573867369
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-10-21 16:42:41 -0300

    feat: add more utils functions

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
 import cv2
 
-
 def read_file (filename):
   img = cv2.imread('assets\\' + filename, 0)
   return img
 
+def read_files(filenames):
+  files = []
+  
+  for filename in filenames:
+    files.append(read_file(filename))
+
+  return files
+
 def save_file(filename, img):
   cv2.imwrite(filename, img)
+
+def save_files(images):
+  for image in images:
+    save_file(image[0], image[1])
