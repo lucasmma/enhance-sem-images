394855ed6085483044438421150b8ad00dd9d4bd
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-10-22 15:34:54 -0300

    refactor: update utils functions

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import cv2
+from matplotlib import pyplot as plt
 
 def read_file (filename):
-  img = cv2.imread('assets\\' + filename, 0)
+  img = cv2.imread('assets\\' + filename, cv2.IMREAD_GRAYSCALE)
   return img
 
 def read_files(filenames):
@@ -18,3 +19,13 @@ def save_file(filename, img):
 def save_files(images):
   for image in images:
     save_file(image[0], image[1])
+
+def show_images(images):
+  for i in range(0, len(images)):
+    cv2.imshow('image {}'.format(i), images[i])
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+
+def show_histogram(img):
+  plt.hist(img.ravel(),256,[0,256])
+  plt.show()
