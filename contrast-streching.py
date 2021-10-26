6cf48afb3e5e572c4fb549242e68dcaec97ce639
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-10-26 17:25:14 -0300

    refactor: adapting contrast streching to multiple files

--- contrast-streching.py
+++ contrast-streching.py
@@ -1,11 +1,11 @@
-from utils import read_file, show_images, show_histogram
+from utils import read_file, show_images, show_histogram, save_file
 import cv2
 import numpy as np
 
+filenames = ['planta.jpg', 'polen_big.jpg', 'polen.jpg', 'tartigrado.jpg', 'texture.jpg', 'tungstenio.png']
+
 def constrast_streching (img):
   counts = np.bincount(img.flatten())
-  # high = np.argmax(counts)
-  # low = np.argmin(counts)
   high = np.max(img)
   low = np.min(img)
   alpha = 255 / (high - low)
@@ -18,11 +18,13 @@ def constrast_streching (img):
 
 
 def main():
-  img = read_file("planta.jpg")
-  new_img = constrast_streching(img)
-  # show_histogram(img)
-  # show_histogram(new_img) 
-  show_images([img, new_img])
+  for filename in filenames:
+    img = read_file(filename)
+    new_img = constrast_streching(img)
+    # show_histogram(img)
+    # show_histogram(new_img) 
+    # show_images([img, new_img])
+    save_file(r"results\\contrast-streching\\" + filename, new_img)
 
 
 if __name__ == "__main__":
