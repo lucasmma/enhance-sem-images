ea13e7ad33d9e10661e12446e3cb008e9ff76f5f
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-10-26 16:31:20 -0300

    refactor: change functions to utils

--- histogram-statistics.py
+++ histogram-statistics.py
@@ -1,30 +1,25 @@
-from utils import read_file, show_images
+from utils import read_file, show_images, neighbors, mean_standard_deviation
 import cv2
 import numpy as np
-
-def neighbors(img, radius, rowNumber, columnNumber):
-     return [[img[i][j] if  i >= 0 and i < len(img) and j >= 0 and j < len(img[0]) else 0
-                for j in range(columnNumber-1-radius, columnNumber+radius)]
-                    for i in range(rowNumber-1-radius, rowNumber+radius)]
-
-def mean_standard_deviation(img):
-  mean = img.mean()
-  standard_deviation = np.std(img)
-  return mean, standard_deviation
 
 def histogram_statistics (img, E, k0, k1, k2):
   new_img = []
   global_mean, standard_deviation = mean_standard_deviation(img)
   
-  for x in range(len(img)):
-    row = img[x]
+  for x, row in enumerate(img):
     new_row = []
-    for y in range(len(row)):
-      pixel = row[y]
+    for y, pixel in enumerate(row):
       local_image = neighbors(img, 1, x, y)
       local_mean, local_standard_deviation = mean_standard_deviation(np.uint8(local_image))
-      if (local_mean <= k0 * global_mean) and (k1 * standard_deviation <= local_standard_deviation and local_standard_deviation <= k2 * standard_deviation):
+      # if x == len(img) - 2 and y == 1:
+        # print(local_image)
+        # print(img)
+        # print(local_mean, local_standard_deviation)
+        # return
+      if (local_mean <= (k0 * global_mean)) and ((k1 * standard_deviation) <= local_standard_deviation and local_standard_deviation <= (k2 * standard_deviation)):
+        # print(x, y, "->", pixel, E * pixel)
         new_row.append(E * pixel)
+        # new_row.append(255)
       else:
         new_row.append(pixel)
     new_img.append(new_row)
@@ -34,7 +29,9 @@ def histogram_statistics (img, E, k0, k1, k2):
 
 def main():
   img = read_file("tungstenio.png")
-  new_img = histogram_statistics(img, E=5.5, k0=0.4, k1=0.08,  k2=0.4)
+  # new_img = histogram_statistics(img, E=5.5, k0=0.4, k1=0.04,  k2=0.95)
+  # new_img = histogram_statistics(img, E=5.5, k0=0.95, k1=0.040,  k2=0.95)
+  new_img = histogram_statistics(img, E=3, k0=0.2, k1=0.01,  k2=0.8)
   show_images([img, new_img])
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import cv2
 from matplotlib import pyplot as plt
+import numpy as np
 
 def read_file (filename):
   img = cv2.imread('assets\\' + filename, cv2.IMREAD_GRAYSCALE)
@@ -32,3 +33,14 @@ def show_histogram(img):
 
 def resize_img(img, width, height):
   cv2.resize(img, (width, height))
+
+def neighbors(img, radius, rowNumber, columnNumber):
+     return [[img[i][j] if  i >= 0 and i < len(img) and j >= 0 and j < len(img[0]) else 0
+                for j in range(columnNumber-radius, columnNumber+radius+1)]
+                    for i in range(rowNumber-radius, rowNumber+radius+1)]
+
+def mean_standard_deviation(img):
+  mean = img.mean()
+  # standard_deviation = ((((np.std(img)) ** 2) * len(img) * len(img[0])) / ((len(img) * len(img[0])) - 1) ) ** (1/2)
+  standard_deviation = np.std(img)
+  return mean, standard_deviation
