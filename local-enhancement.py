192de0a33622d571e61b3b9cf2392ff967330776
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-10-26 16:31:54 -0300

    refactor: change localEnhancement function that fits on article

--- local-enhancement.py
+++ local-enhancement.py
@@ -1,17 +1,31 @@
-from utils import read_file, show_images
+from utils import read_file, show_images, neighbors, mean_standard_deviation
 import cv2
 import numpy as np
 
-def localEnhancement (img):
-  clahe = cv2.createCLAHE(clipLimit=1.8, tileGridSize=(8, 8))
-  new_img = clahe.apply(img)
-  return new_img
+def localEnhancement (img, k, b):
+  new_img = []
+  global_mean, standard_deviation = mean_standard_deviation(img)
+  
+  for x, row in enumerate(img):
+    new_row = []
+    for y, pixel in enumerate(row):
+      local_image = neighbors(img, 1, x, y)
+      local_mean, local_standard_deviation = mean_standard_deviation(np.uint8(local_image))
+
+      a_value = k * global_mean / (local_standard_deviation + b)
+
+      new_value = a_value *  (pixel - local_mean) + local_mean
+
+      new_row.append(new_value)
+    new_img.append(new_row)
+
+  return np.uint8(new_img)
 
 
 def main():
-  img = read_file("polen_big.jpg")
+  img = read_file("tartigrado.jpg")
 
-  new_img = localEnhancement(img)
+  new_img = localEnhancement(img, 0.23, 2)
 
   show_images([img, new_img])
 
