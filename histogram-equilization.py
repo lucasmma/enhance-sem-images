33893f289628ea016ab1e641d684617d87a857e9
Lucas Mendonça <lucasmmamaral@gmail.com>
2021-10-26 17:28:05 -0300

    refactor: adapting histogram-equilization to multiple images

--- histogram-equilization.py
+++ histogram-equilization.py
@@ -1,6 +1,8 @@
-from utils import read_file, show_images, show_histogram
+from utils import read_file, show_images, show_histogram, save_file
 import cv2
 import numpy as np
+
+filenames = ['planta.jpg', 'polen_big.jpg', 'polen.jpg', 'tartigrado.jpg', 'texture.jpg', 'tungstenio.png']
 
 def equilize_histogram (img):
   new_image = cv2.equalizeHist(img)
@@ -8,12 +10,13 @@ def equilize_histogram (img):
 
 
 def main():
-  img = read_file("planta.jpg")
-  new_img = equilize_histogram(img)
-  show_histogram(img)
-  show_histogram(new_img)
-  show_images([img, new_img])
-
+  for filename in filenames:
+    img = read_file(filename)
+    new_img = equilize_histogram(img)
+    # show_histogram(img)
+    # show_histogram(new_img)
+    # show_images([img, new_img])
+    save_file(r"results\\histogram-equilization\\" + filename, new_img)
 
 if __name__ == "__main__":
     main()
